36d758632866abd1c813bc34240471167ddc1435
blessedb1 <blessingetim6667@gmail.com>
2023-07-17 07:50:51 -0700

    completed tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,20 @@
 #!/usr/bin/node
-
+/* Task 6 -script that computes the number of tasks completed by user id. */
+/*  module request */
 const request = require('request');
-const url = process.argv[2];
-
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    const completed = {};
-    const tasks = JSON.parse(body);
-    for (const i in tasks) {
-      const task = tasks[i];
-      if (task.completed === true) {
-        if (completed[task.userId] === undefined) {
-          completed[task.userId] = 1;
-        } else {
-          completed[task.userId]++;
-        }
+/* The first argument is the API URL: https://jsonplaceholder.typicode.com/todos */
+request(process.argv[2], function (error, response, body) {
+  if (error) throw error;
+  /* computes the number of tasks completed by user id */
+  const users = {};
+  for (const task of JSON.parse(body)) {
+    if (task.completed) {
+      if (users[task.userId]) {
+        users[task.userId]++;
+      } else {
+        users[task.userId] = 1;
       }
     }
-    console.log(completed);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
   }
+  console.log(users);
 });
